f4516ebba9f6ad1263c84a99ffffcebd9b5c45a6
Riddhi-chavan <riddhic164@gmail.com>
2024-01-23 02:10:56 +0530

    Removed Props in getMessage

--- src/app/api/getMessage/route.ts
+++ src/app/api/getMessage/route.ts
@@ -3,7 +3,7 @@ import redis from "../../../../radis";
 import { Message } from "../../../../typings";
 import { NextResponse } from "next/server";
 
-export async function GET(request:  Request) {
+export async function GET() {
     try {
         const messages = await redis.hgetall('message');
 
